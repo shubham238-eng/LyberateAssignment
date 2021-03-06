2b51d5321232a6864a41d0d3e930890242d7ec9c
shubham238-eng <59250446+shubham238-eng@users.noreply.github.com>
2021-03-06 22:59:05 +0530

    Update RecyclerAdapter.java

--- app/src/main/java/daily/worker/lyberateassignment/RecyclerAdapter.java
+++ app/src/main/java/daily/worker/lyberateassignment/RecyclerAdapter.java
@@ -11,18 +11,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import daily.worker.lyberateassignment.model.BasicEntity;
+import daily.worker.lyberateassignment.model.SongsEntity;
 import daily.worker.lyberateassignment.model.Title;
-import daily.worker.lyberateassignment.model.songsEntity;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     static private RecyclerAdapter recyclerAdapter= new RecyclerAdapter();
 
-    private List<BasicEntity> listWithHeaders;
+    private List<BasicEntity> basicEntities;
 
     public static RecyclerAdapter getRecyclerAdapter(){return recyclerAdapter;}
 
     public void update_list(List<BasicEntity> listWithHeaders){
-        this.listWithHeaders = listWithHeaders;
+        this.basicEntities = listWithHeaders;
         notifyDataSetChanged();
     }
 
@@ -32,12 +32,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public int getItemViewType(int position) {
-        return listWithHeaders.get(position).getType();
+        return basicEntities.get(position).getType();
     }
 
     @Override
     public int getItemCount() {
-        return listWithHeaders != null ? listWithHeaders.size() : 0;
+        return basicEntities != null ? basicEntities.size() : 0;
     }
 
     @NonNull
@@ -51,12 +51,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
             case BasicEntity.GENERAL:
                 View v1 = inflater.inflate(R.layout.songs, parent,false);
-                viewHolder = new GeneralViewHolder(v1);
+                viewHolder = new songViewHolder(v1);
                 break;
 
             case BasicEntity.HEADER:
                 View v2 = inflater.inflate(R.layout.artist, parent, false);
-                viewHolder = new DateViewHolder(v2);
+                viewHolder = new TitleViewHolder(v2);
                 break;
         }
 
@@ -72,56 +72,51 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         switch (viewHolder.getItemViewType()) {
 
             case BasicEntity.GENERAL:
-                songsEntity generalItem = (songsEntity) listWithHeaders.get(position);
-                GeneralViewHolder generalViewHolder = (GeneralViewHolder) viewHolder;
-                System.out.println(generalItem.toString());
+                SongsEntity songsEntity = (SongsEntity) basicEntities.get(position);
+                songViewHolder viewHolder1 = (songViewHolder) viewHolder;
+                System.out.println(songsEntity.toString());
 
-                //generalViewHolder.tId.setText();
-                generalViewHolder.tName.setText("");
-                generalViewHolder.tracktimemillis.setText("snjsdij");
-                generalViewHolder.primaryGenreName.setText("snjsdij");
-                generalViewHolder.artistName.setText(generalItem.getArtistName());
-                generalViewHolder.artistId.setText("snjsdij");
-
+                //viewHolder1.tId.setText();
+                viewHolder1.tName.setText(Integer.toString(songsEntity.getTrackId()));
+                viewHolder1.track_time_mill.setText(Integer.toString(songsEntity.getTrackTimeMillis()));
+                viewHolder1.primaryGenreName.setText(songsEntity.getPrimaryGenreName());
 
                 break;
 
             case BasicEntity.HEADER:
-                Title dateItem = (Title) listWithHeaders.get(position);
-                DateViewHolder dateViewHolder = (DateViewHolder) viewHolder;
-                dateViewHolder.txtTitle.setText(dateItem.getArtist_Name());
+                Title title = (Title) basicEntities.get(position);
+                TitleViewHolder viewHolder2 = (TitleViewHolder) viewHolder;
+                viewHolder2.txtTitle.setText(title.getArtist_Name() +  "( " + title.getArtist_Id() + " )");
                 break;
         }
     }
 
 
 
-    // ViewHolder for Header
-    static class DateViewHolder extends RecyclerView.ViewHolder {
+    // Header class
+    static class TitleViewHolder extends RecyclerView.ViewHolder {
         protected TextView txtTitle;
 
-        public DateViewHolder(View v) {
+        public TitleViewHolder(View v) {
             super(v);
             this.txtTitle = (TextView) v.findViewById(R.id.textHeader);
 
         }
     }
 
-    // View holder for general row item
-    static class GeneralViewHolder extends RecyclerView.ViewHolder {
+    // songs entity class
+    static class songViewHolder extends RecyclerView.ViewHolder {
 
 
-        TextView tId,tName,tracktimemillis,primaryGenreName,artistName,artistId;
+        TextView tId,tName,track_time_mill,primaryGenreName;
 
-        public GeneralViewHolder(View v) {
+        public songViewHolder(View v) {
             super(v);
 
-            tId=itemView.findViewById(R.id.textTrackId);
-            tName=itemView.findViewById(R.id.textTrackName);
-            tracktimemillis=itemView.findViewById(R.id.textTrackTimeMillis);
-            primaryGenreName=itemView.findViewById(R.id.textPrimaryGenre);
-            artistName=itemView.findViewById(R.id.textArtistName);
-            artistId=itemView.findViewById(R.id.textArtistId);
+            tId=itemView.findViewById(R.id.TrackId);
+            tName=itemView.findViewById(R.id.TrackName);
+            track_time_mill=itemView.findViewById(R.id.TrackTimeMillis);
+            primaryGenreName=itemView.findViewById(R.id.PrimaryGenre);
 
         }
     }
